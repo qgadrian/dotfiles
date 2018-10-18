8bd83c2243f854e6bc5df48800d193e51e32b29c
qgadrian <qgadrian@gmail.com>
2018-10-18 15:34:56 +0200

    Add gpg

---
+++ gpg/install.sh
@@ -0,0 +1,27 @@
+echo "Install GPG"
+
+brew install gpg
+
+# Gen GPG key
+# gpg --full-gen-key
+
+# List created GPG keys
+# gpg --list-secret-keys --keyid-format LONG my@email.sh
+
+# Get the key id, see example:
+# sec   rsa4096/$key_id
+
+# Export key
+# gpg --armor --export
+
+# Set key to sign git commits
+# git config --global user.signingkey $key_id
+
+# Set global signed commits
+# git config --global commit.gpgsign true
+
+# Set the gpg command (might not be neccesary)
+# git config --global gpg.program gpg
+
+# Set gpg tty
+# export GPG_TTY=$(tty)
