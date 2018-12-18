a64a291e299929e9284dbf563e844e458ca4a7dd
qgadrian <qgadrian@gmail.com>
2018-12-18 18:57:02 +0100

    Updsate docker with useful commands

--- docker/install.sh
+++ docker/install.sh
@@ -4,3 +4,12 @@ brew cask install docker
 
 # docker alias to remove stopped containers
 # docker rm -v $(docker ps -a -q -f status=exited)
+
+# docker alias to stop all containers
+# docker stop $(docker ps -a -q)
+
+# stop all exited or dead containers
+# docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
+
+# remove images
+# docker rmi $(docker images |grep '<none>')
