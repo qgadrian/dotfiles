f4653f99a3fe1a8dd9541e6cade0dbee3c306e18
qgadrian <qgadrian@gmail.com>
2018-12-18 18:56:23 +0100

    Update vim

---
+++ vim/.vimrc
@@ -0,0 +1,7 @@
+" fix meta-keys which generate <Esc>a .. <Esc>z
+let c='a'
+while c <= 'z'
+  exec "set <M-".toupper(c).">=\e".c
+  exec "imap \e".c." <M-".toupper(c).">"
+  let c = nr2char(1+char2nr(c))
+endw
---
+++ vim/config.sh
@@ -0,0 +1,24 @@
+echo "Install vimrc"
+
+brew install macvim
+
+#brew cask install xquartz
+#brew install vim --with-client-server
+
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Needed by 'youcompleteme'
+brew install cmake
+# mkdir YouCompleteMe/ycmbuild
+# cd YouCompleteMe/ycmbuild
+# cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/
+# make ycm_core
+
+# If python crashses at start: ./install.py --clang-completer
+
+ln -s .vimrc_suggestion ~/.vimrc
+
+
