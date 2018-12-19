b95cc43d8384ce51e2232cd53ce6a4e880352cd8
Adrian Quintas <adrianquintas@Adrians-MacBook-Pro.local>
2018-12-19 08:32:10 +0100

    Add missing install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 source ./brew/install.sh
 
+source ./asdf/install.sh
+
 source ./zsh/install.sh
 
 source ./git/config.sh
 
 source ./docker/install.sh
-
-source ./asdf/install.sh
 
 source ./iterm2/install.sh
 
@@ -15,3 +15,7 @@ source ./plugins/install.sh
 source ./tools/install.sh
 
 source ./ansible/install.sh
+
+source ./vim/config.sh
+
+echo "Done setting up the system"
