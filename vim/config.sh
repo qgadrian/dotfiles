36f9800285c90571dfa19adf9fcba35ba07a7c4a
Adrian Quintas <adrianquintas@Adrians-MacBook-Pro.local>
2018-12-19 14:09:58 +0100

    Update vim config

--- vim/config.sh
+++ vim/config.sh
@@ -10,21 +10,27 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Needed by 'youcompleteme'
-brew install cmake
-# mkdir YouCompleteMe/ycmbuild
-# cd YouCompleteMe/ycmbuild
-# cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/
-# make ycm_core
-
 # If python crashses at start: ./install.py --clang-completer
 
 mv ~/.vimrc ~/.vimrc.old.deleteme
-# ln -s .vimrc_suggestion ~/.vimrc
-cp .vimrc_suggestion ~/.vimrc
+ln -s $(pwd)/.vimrc_suggestion ~/.vimrc
 
 mkdir ~/.vim/backup
 mkdir ~/.vim/swp
 mkdir ~/.vim/undo
 
 vim +PluginInstall +qall
+
+# Compile and install YCM
+
+# Needed by 'youcompleteme'
+brew install cmake
+
+# mkdir YouCompleteMe/ycmbuild
+mkdir ~/.vim/bundle/YouCompleteMe/ycmbuild
+# cd YouCompleteMe/ycmbuild
+# cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/
+cmake -G "Unix Makefiles" -B ~/.vim/bundle/YouCompleteMe/ycmbuild -S ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/
+# make ycm_core
+make -C ~/.vim/bundle/YouCompleteMe/ycmbuild ycm_core
+
