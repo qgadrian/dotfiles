946a4c477ca118ddea3e203c147ebce1b38345ba
qgadrian <qgadrian@users.noreply.github.com>
2019-06-15 14:58:47 +0200

    Add elm to asdf

--- asdf/install.sh
+++ asdf/install.sh
@@ -30,21 +30,25 @@ asdf plugin-add ruby
 asdf plugin-add terraform
 asdf plugin-add haskell
 asdf plugin-add rust
+asdf plugin-add elm
 
 ELIXIR_VERSION=1.8
 ERLANG_VERSION=21.3
 HASKELL_VERSION=8.6.5
 RUST_VERSION=1.34.1
 TERRAFORM_VERSION=0.12.0
+ELM_VERSION=0.19.0
 
 asdf install elixir $ELIXIR_VERSION-otp-21
 asdf install erlang $ERLANG_VERSION
 asdf install terraform $TERRAFORM_VERSION
 asdf install haskell $HASKELL_VERSION
 asdf install rust $RUST_VERSION
+asdf install elm $ELM_VERSION
 
 asdf global erlang $ERLANG_VERSION
 asdf global elixir $ELIXIR_VERSION-otp-21
 asdf global terraform $TERRAFORM_VERSION
 asdf global haskell $HASKELL_VERSION
 asdf global rust $RUST_VERSION
+asdf global elm $ELM_VERSION
