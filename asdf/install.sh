a741d912d9679d10638083c349cac3da6daefc77
qgadrian <qgadrian@users.noreply.github.com>
2019-09-18 09:16:52 +0200

    add postgres to asdf

--- asdf/install.sh
+++ asdf/install.sh
@@ -31,6 +31,7 @@ asdf plugin-add terraform
 asdf plugin-add haskell
 asdf plugin-add rust
 asdf plugin-add elm
+asdf plugin-add postgres
 
 ELIXIR_VERSION=1.8
 ERLANG_VERSION=21.3
@@ -38,6 +39,7 @@ HASKELL_VERSION=8.6.5
 RUST_VERSION=1.34.1
 TERRAFORM_VERSION=0.12.0
 ELM_VERSION=0.19.0
+POSTGRES_VERSION=11.5
 
 asdf install elixir $ELIXIR_VERSION-otp-21
 asdf install erlang $ERLANG_VERSION
@@ -45,6 +47,7 @@ asdf install terraform $TERRAFORM_VERSION
 asdf install haskell $HASKELL_VERSION
 asdf install rust $RUST_VERSION
 asdf install elm $ELM_VERSION
+asdf install postgres $POSTGRES_VERSION
 
 asdf global erlang $ERLANG_VERSION
 asdf global elixir $ELIXIR_VERSION-otp-21
@@ -52,3 +55,4 @@ asdf global terraform $TERRAFORM_VERSION
 asdf global haskell $HASKELL_VERSION
 asdf global rust $RUST_VERSION
 asdf global elm $ELM_VERSION
+asdf global postgres $POSTGRES_VERSION
