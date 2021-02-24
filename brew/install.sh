2a3517603a454cf4cbd3f292e6ad01d6c403efc3
Adrián Quintás <qgadrian@users.noreply.github.com>
2021-02-24 13:45:23 +0100

    Fix brew install

--- brew/install.sh
+++ brew/install.sh
@@ -2,4 +2,7 @@ echo "Install brew"
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew tap caskroom/cask
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$(whoami)/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+
+brew tap homebrew/cask
