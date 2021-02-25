02f497b669727aafbb06f999fe712d323cc4d4ab
qgadrian <qgadrian@users.noreply.github.com>
2021-02-25 20:14:50 +0100

    Ensure GPG is imported before start

--- before_setup.sh
+++ before_setup.sh
@@ -1,7 +1,13 @@
 echo "Setting up an encrypted environment"
 
-# See https://github.com/AGWA/git-crypt
-
 brew install git-crypt
 
-git-crypt unlock
+read -p "Is the GPG key imported? " -n 1 -r; echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git-crypt unlock
+    echo "Repository unlocked"
+  else
+    echo "Aborted, please import GPG key"
+  fi
+
+# See https://github.com/AGWA/git-crypt
