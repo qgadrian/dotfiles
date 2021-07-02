df3a77c7d2427eea9cda10de6e24c6487c7973e7
qgadrian <qgadrian@users.noreply.github.com>
2021-07-02 19:01:20 +0200

    Add slack terminal example installation
    It won't be used at the moment

---
+++ tools/slack/slack-terminal.sh
@@ -0,0 +1,5 @@
+# https://github.com/erroneousboat/slack-term
+git clone git@github.com:erroneousboat/slack-term.git
+cd slack-term
+make build-mac
+# ./bin/slack-term -config ~/.config/slack-term/config
