49c5664eb2a039a1dcf76b02239821a5aaf0b59c
qgadrian <qgadrian@users.noreply.github.com>
2021-07-12 09:15:26 +0200

    Add more mac tweaks
    Some of them as example, in case they are worth to enabled them in the
    future

--- mac/config.sh
+++ mac/config.sh
@@ -13,6 +13,24 @@ echo "Disable auto rearrange spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
 # killall Dock
 
+# Hide removable media on desktop
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+
+echo "Install command line tools"
+xcode-select --install
+
+# Automatic Restart on System Freeze
+#sudo systemsetup -setrestartfreeze on
+
+# Enable Firewall
+#sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+
+# Show All File Extensions
+#defaults write -g AppleShowAllExtensions -bool true
+
+# Show Full Path in Finder Title
+#defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
 source $(pwd)/mac/plugins/install.sh
 
 source $(pwd)/mac/window_management/install.sh
