851ae4c45a297330621ebb9f345962c0226e81b6
qgadrian <qgadrian@users.noreply.github.com>
2021-08-30 13:19:14 +0200

    Add `typescript` and its language server globally
    This is required to make the LSP servers in nvim work.

--- lang/yarn/config.sh
+++ lang/yarn/config.sh
@@ -4,3 +4,6 @@ yarn config set prefix ~/.yarn
 
 # eslint_d it's being used to improve lint perfomance
 yarn global add eslint_d
+
+# add typescript support and language server globally
+yarn global add typescript typescript-language-server
