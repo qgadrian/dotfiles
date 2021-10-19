ffa335aa8a60f7cc862c48d17d6098a86c57b2bf
qgadrian <qgadrian@users.noreply.github.com>
2021-10-19 09:16:24 +0200

    Enable auto format on `eslint`
    https://github.com/williamboman/nvim-lsp-installer/blob/main/lua/nvim-lsp-installer/servers/eslint/README.md

--- vim/config/nvim/lua/lsp/config.lua
+++ vim/config/nvim/lua/lsp/config.lua
@@ -48,10 +48,17 @@ lsp_installer.on_server_ready(function(server)
   -- nvim-cmp capabilities
   opts.capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-  -- (optional) Customize the options passed to the server
-  -- if server.name == "tsserver" then
-  --     opts.root_dir = function() ... end
-  -- end
+  if server.name == "eslint" then
+    opts.on_attach = function (client, bufnr)
+      -- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
+      -- the resolved capabilities of the eslint server ourselves!
+      client.resolved_capabilities.document_formatting = true
+      on_attach(client, bufnr)
+    end
+    opts.settings = {
+      format = { enable = true }, -- this will enable formatting
+    }
+  end
 
   -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
   server:setup(opts)
