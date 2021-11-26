4a4c3fa832f0a797282a3c1facfc65be15885def
qgadrian <qgadrian@users.noreply.github.com>
2021-11-26 21:39:41 +0100

    Update automated AppStore installs

--- tools/mas/install.sh
+++ tools/mas/install.sh
@@ -8,4 +8,8 @@ read -p "Did you sign in in the AppStore? " -n 1 -r; echo
     mas install 963507809
     # Quick calendar
     mas install 1004514425
+    # Darkroom
+    mas install 953286746
+    # Pixelmator Pro
+    mas install 1289583905
 fi
