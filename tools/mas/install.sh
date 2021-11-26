c10a66c1af8c7c251861064cd086044d58802549
qgadrian <qgadrian@users.noreply.github.com>
2021-11-26 18:46:55 +0100

    Add info for `mas` to sign in the AppStore first

--- tools/mas/install.sh
+++ tools/mas/install.sh
@@ -1,4 +1,11 @@
 brew install mas
 
-# Fast RAW preview
-mas install 963507809
+echo "Please sign in in the AppStore first"
+
+read -p "Did you sign in in the AppStore? " -n 1 -r; echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Fast RAW preview
+    mas install 963507809
+    # Quick calendar
+    mas install 1004514425
+fi
