cfc5b84502446150fc96d012a954e056deac1d06
qgadrian <qgadrian@users.noreply.github.com>
2022-01-12 22:49:48 +0100

    Use lua API for telescope mappings

--- vim/config/nvim/lua/plugins/telescope.lua
+++ vim/config/nvim/lua/plugins/telescope.lua
@@ -15,3 +15,18 @@ require('telescope').setup {
     }
   }
 }
+
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>fr', '<cmd>Telescope resume<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>ca', "<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>", {})
+
+-- TODO
+-- Remove `vim/config/vim/telescope.vim`
+-- See https://github.com/neovim/neovim/pull/16591
+--
+-- vim.keymap.set('n', '<leader>ca', function()
+--   require('telescope.builtin').lsp_code_actions()
+-- end, { expr = true })
