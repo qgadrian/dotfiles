b86373fa6c7eab57103690b0e5c22313032e981e
qgadrian <qgadrian@users.noreply.github.com>
2022-08-06 16:19:21 +0200

    Update pam install script

--- mac/pam.d/pam_touch_id.sh
+++ mac/pam.d/pam_touch_id.sh
@@ -2,4 +2,4 @@ echo "Install PAM plugin for Touch ID"
 
 # Credits: https://sixcolors.com/post/2020/11/quick-tip-enable-touch-id-for-sudo/
 
-sudo sed -i '' '3i\'$'\nauth\tsufficient\t\pam_tid.so\n' /etc/pam.d/sudo
+sudo sed -i '' '2i\'$'\nauth       sufficient     pam_tid.so\n' /etc/pam.d/sudo
