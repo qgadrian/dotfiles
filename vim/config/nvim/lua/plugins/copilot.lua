b0c3efcceceba32b4725006f90afd787514a5022
qgadrian <qgadrian@users.noreply.github.com>
2023-10-05 12:05:37 +0200

    Fix copilot mappings

--- vim/config/nvim/lua/plugins/copilot.lua
+++ vim/config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,10 @@
 -- update the copilot maps for next suggestion
 vim.api.nvim_set_keymap('i', '<C-;>', '<Plug>(copilot-next)', { noremap = false })
 vim.api.nvim_set_keymap('i', '<C-,>', '<Plug>(copilot-previous)', { noremap = false })
-vim.api.nvim_set_keymap('i', '<C-x>', '<Plug>(copilot-cancel)', { noremap = false })
+vim.api.nvim_set_keymap('i', '<C-x>', '<Plug>(copilot-dismiss)', { noremap = false })
 -- accept copilot suggestion
-vim.api.nvim_set_keymap('i', '<C-g>', '<Plug>(copilot-accept)', { noremap = false })
+vim.api.nvim_set_keymap('i', '<C-g>', '<Plug>(copilot-suggest)', { noremap = false })
+
+vim.g.copilot_no_tab_map = true;
+vim.g.copilot_assume_mapped = true;
+vim.g.copilot_tab_fallback = "";
