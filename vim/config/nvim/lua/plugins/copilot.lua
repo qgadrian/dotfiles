cb8a38966f330fde4be44494b621c53aaee57033
qgadrian <qgadrian@users.noreply.github.com>
2023-11-14 17:05:28 +0100

    Add Copilot mapping to suggest something

--- vim/config/nvim/lua/plugins/copilot.lua
+++ vim/config/nvim/lua/plugins/copilot.lua
@@ -4,6 +4,8 @@ vim.api.nvim_set_keymap('i', '<C-,>', '<Plug>(copilot-previous)', { noremap = fa
 vim.api.nvim_set_keymap('i', '<C-x>', '<Plug>(copilot-dismiss)', { noremap = false })
 -- accept copilot suggestion
 vim.api.nvim_set_keymap('i', '<C-g>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+-- suggest even if copilot is not active for the current buffer
+vim.api.nvim_set_keymap('i', '<C-s>', 'copilot#Suggest()', { silent = true, expr = true })
 
 vim.g.copilot_no_tab_map = true;
 vim.g.copilot_assume_mapped = true;
