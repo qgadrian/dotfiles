8c0038c267983ebd711cd85a8732e2b28c18dde3
qgadrian <qgadrian@users.noreply.github.com>
2023-11-15 19:36:40 +0100

    Add `Monospace Neon` font to Kitty conf

--- mac/fonts/install.sh
+++ mac/fonts/install.sh
@@ -1,3 +1,7 @@
 echo "Install fonts"
 
 find . -type f -exec sudo cp '{}' ~/Library/Fonts/ \;
+
+wget https://github.com/githubnext/monaspace/releases/download/v1.000/monaspace-v1.000.zip
+unzip monaspace-v1.000.zip
+./monospace-v1.000/util/install_macos.sh
