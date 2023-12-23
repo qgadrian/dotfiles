4c4e209131e628d64cd1ad007cb9a442bec74efc
Adrian Quintas <qgadrian@users.noreply.github.com>
2023-12-23 20:13:10 +0100

    Install Monaspace patched font with nerd fonts
    https://www.nerdfonts.com/font-downloads

--- mac/fonts/install.sh
+++ mac/fonts/install.sh
@@ -1,7 +1,15 @@
 echo "Install fonts"
 
-find . -type f -exec sudo cp '{}' ~/Library/Fonts/ \;
+# find . -type f -exec sudo cp '{}' ~/Library/Fonts/ \;
 
 # monaspace: https://github.com/githubnext/monaspace#macos
-brew tap homebrew/cask-fonts
-brew install font-monaspace
+# regular fonts, not nerd fonts
+# brew tap homebrew/cask-fonts
+# brew install font-monaspace
+
+# patched nerd fonts
+CURRENT_DIR=$(pwd)/mac/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Monaspace.zip -O $CURRENT_DIR/Monaspace.zip
+unzip $CURRENT_DIR/Monaspace.zip -d $CURRENT_DIR/Monaspace
+cp $CURRENT_DIR/Monaspace/*.otf ~/Library/Fonts/
+rm -rf $CURRENT_DIR/Monaspace.zip $CURRENT_DIR/Monaspace
