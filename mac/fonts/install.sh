ece7ffe64ee3c75716857397ffa0f93dcb1fcffb
Adrián Q <qgadrian@users.noreply.github.com>
2023-12-23 10:52:00 +0100

    Ensure wget is available before installing fonts

--- mac/fonts/install.sh
+++ mac/fonts/install.sh
@@ -1,5 +1,7 @@
 echo "Install fonts"
 
+brew install wget 
+
 find . -type f -exec sudo cp '{}' ~/Library/Fonts/ \;
 
 wget https://github.com/githubnext/monaspace/releases/download/v1.000/monaspace-v1.000.zip
