9d993a616486164d14ac185093d70dfd4252df32
Adrian Quintas <qgadrian@users.noreply.github.com>
2023-12-23 13:14:02 +0100

    Avoid reinstalling brew when it is already present

--- brew/install.sh
+++ brew/install.sh
@@ -1,6 +1,13 @@
-echo "Install brew"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ ! -d /opt/homebrew/ ]; then
+  echo "Install brew"
+
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [ -d /opt/homebrew/ ]; then
+  echo "brew is already installed, skipping"
+fi
 
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$(whoami)/.zprofile
 eval $(/opt/homebrew/bin/brew shellenv)
