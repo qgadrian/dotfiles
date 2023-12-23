aba39f52afb3012f9c2c8e87f78300e4e2ddb1a6
Adrian Quintas <qgadrian@users.noreply.github.com>
2023-12-23 14:04:54 +0100

    Don't use fixed versions of App stores to install

--- tools/mas/install.sh
+++ tools/mas/install.sh
@@ -5,11 +5,10 @@ echo "Please sign in in the AppStore first"
 read -p "Did you sign in in the AppStore? " -n 1 -r; echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     # Fast RAW preview
-    mas install 963507809
-    # Quick calendar
-    mas install 1004514425
-    # Darkroom
-    mas install 953286746
-    # Pixelmator Pro
-    mas install 1289583905
+    mas install $(mas search "raw right away" | head -n 1 | awk '{print $1}')
+    # Quick View calendar
+    mas install $(mas search "quick view calendar" | head -n 1 | awk '{print $1}')
+    # Pixelmator Apps
+    mas install $(mas search "pixelmator pro" | head -n 1 | awk '{print $1}')
+    mas install $(mas search "photomator" | head -n 1 | awk '{print $1}')
 fi
