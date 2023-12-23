587a72c6216bab5e2841cfbf8238bfb4f08fe021
Adrian Quintas <qgadrian@users.noreply.github.com>
2023-12-23 20:23:24 +0100

    Use Meslo mono font for kitty
    It's way better than monaspice

--- mac/fonts/install.sh
+++ mac/fonts/install.sh
@@ -9,7 +9,17 @@ echo "Install fonts"
 
 # patched nerd fonts
 CURRENT_DIR=$(pwd)/mac/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Monaspace.zip -O $CURRENT_DIR/Monaspace.zip
-unzip $CURRENT_DIR/Monaspace.zip -d $CURRENT_DIR/Monaspace
+
+# Monaspace Nerd Font (Monaspice)
+echo "Install Monaspace Nerd Font (Monaspice)"
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Monaspace.zip -O $CURRENT_DIR/Monaspace.zip
+unzip -q $CURRENT_DIR/Monaspace.zip -d $CURRENT_DIR/Monaspace
 cp $CURRENT_DIR/Monaspace/*.otf ~/Library/Fonts/
 rm -rf $CURRENT_DIR/Monaspace.zip $CURRENT_DIR/Monaspace
+
+# Meslo Nerd Font (MesloLG)
+echo "Install Meslo Nerd Font (MesloLG)"
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Meslo.zip -O $CURRENT_DIR/Meslo.zip
+unzip -q $CURRENT_DIR/Meslo.zip -d $CURRENT_DIR/Meslo
+cp $CURRENT_DIR/Meslo/*.ttf ~/Library/Fonts/
+rm -rf $CURRENT_DIR/Meslo.zip $CURRENT_DIR/Meslo
