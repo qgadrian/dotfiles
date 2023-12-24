e5d178f175e439c7dbcf0019d93dac571f24f21c
Adrian Quintas <qgadrian@users.noreply.github.com>
2023-12-24 11:19:21 +0100

    Refactor AppStore apps installation script
    This change will force to confirm which app to install. This avoids the case the order is not as expected (this actually happened) and the wrong app is installed.

--- tools/mas/install.sh
+++ tools/mas/install.sh
@@ -1,17 +1,26 @@
-brew install mas
+if ! command -v mas &> /dev/null
+then
+  echo "mas could not be found, installing"
+  brew install mas
+fi
 
+function search_and_install() {
+  SELECTED_APP=$(mas search "$1" | peco | head -n 1 | awk '{print $1}')
+
+  mas install $SELECTED_APP
+}
+
+echo "You will need to confirm which apps from the store you want to install"
 echo "Please sign in in the AppStore first"
 
 read -p "Did you sign in in the AppStore? " -n 1 -r; echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    # Fast RAW preview
-    mas install $(mas search "raw right away" | head -n 1 | awk '{print $1}')
-    # Quick View calendar
-    mas install $(mas search "quick view calendar" | head -n 1 | awk '{print $1}')
-    # Pixelmator Apps
-    mas install $(mas search "pixelmator pro" | head -n 1 | awk '{print $1}')
-    mas install $(mas search "photomator" | head -n 1 | awk '{print $1}')
-    mas install $(mas search "Slack for Desktop" | head -n 1 | awk '{print $1}')
-    mas install $(mas search "Telegram" | head -n 1 | awk '{print $1}')
-    mas install $(mas search "Infuse Video Player" | head -n 1 | awk '{print $1}')
+    search_and_install "raw right away"
+    search_and_install "quick view calendar"
+    search_and_install "Pixelmator Pro"
+    search_and_install "Photomator"
+    search_and_install "Slack for Desktop"
+    search_and_install "Infuse Video Player"
+    search_and_install "WhatsApp Desktop"
+    search_and_install "Telegram"
 fi
