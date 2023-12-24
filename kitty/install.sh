e09ee0844d9041478d295b601c7fe60d8c162ba7
Adrian Quintas <qgadrian@users.noreply.github.com>
2023-12-24 19:24:39 +0100

    Replace Kitty icon
    Includes better installation experience (restarts the Docker
    automatically to see the new icon)

--- kitty/install.sh
+++ kitty/install.sh
@@ -1,20 +1,23 @@
 echo "Install kitty"
 
-brew install kitty
+if ! command -v kitty &> /dev/null; then
+  brew install kitty
+fi
 
 ln -sf $(pwd)/kitty/.config/kitty ~/.config/
 
 # set custom icon for kitty app
 # https://sw.kovidgoyal.net/kitty/faq/#i-do-not-like-the-kitty-icon
 # https://github.com/k0nserv/kitty-icon
-KITTY_CONFIG_PATH=~/.config/kitty
-KITTY_ICNS_PATH=$KITTY_CONFIG_PATH/kitty.app.icns
-KITTY_PNG_PATH=$KITTY_CONFIG_PATH/kitty.app.png
+CONFIG_PATH=~/.config/kitty
+ICNS_PATH=$CONFIG_PATH/kitty.app.icns
+PNG_PATH=$CONFIG_PATH/kitty.app.png
+ICON_NAME=neue_toxic
 
-wget https://github.com/k0nserv/kitty-icon/raw/main/build/outrun.icns -O $KITTY_ICNS_PATH
-wget https://github.com/k0nserv/kitty-icon/blob/7f631a61bcbdfb268cdf1c97992a5c077beec9d6/build/outrun.iconset/icon_512x512%402x.png -O $KITTY_PNG_PATH
+wget https://github.com/k0nserv/kitty-icon/raw/main/build/$ICON_NAME.icns -O $ICNS_PATH
+wget https://raw.githubusercontent.com/k0nserv/kitty-icon/main/build/$ICON_NAME.iconset/icon_256x256.png -O $PNG_PATH
 
-kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' $KITTY_ICNS_PATH
-kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' $KITTY_PNG_PATH /Applications/kitty.app
+kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' $ICNS_PATH
+kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' $PNG_PATH /Applications/kitty.app
 
-# rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
+rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
