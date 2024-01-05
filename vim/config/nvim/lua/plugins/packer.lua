af254e79040dcb33a31bf25d222ba600f82b2bfa
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-01-05 16:44:29 +0100

    format file

--- vim/config/nvim/lua/plugins/packer.lua
+++ vim/config/nvim/lua/plugins/packer.lua
@@ -88,8 +88,8 @@ require('packer').startup({
         { 'nvim-tree/nvim-web-devicons' },
       }
     }
-    use { 
-      'nvim-telescope/telescope-fzf-native.nvim', 
+    use {
+      'nvim-telescope/telescope-fzf-native.nvim',
       run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
       config = function()
         require("telescope").load_extension("fzf")
