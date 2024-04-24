0011b502e3921a620266fff030a3b7aa7edc70d7
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-04-24 20:37:37 +0200

    Update tint file

--- vim/config/nvim/lua/plugins/tint.lua
+++ vim/config/nvim/lua/plugins/tint.lua
@@ -1,7 +1,8 @@
 require("tint").setup({
-  tint_background_colors = false,  -- Tint background portions of highlight groups
-  tint = -100,  -- Darken colors, use a positive value to brighten
-  saturation = 0,
+  tint_background_colors = false, -- Tint background portions of highlight groups
+  tint = -45,                     -- Darken colors, use a positive value to brighten
+  saturation = 0.23,
+  highlight_ignore_patterns = { "WinSeparator", "Status.*" },
   window_ignore_function = function(winid)
     local bufid = vim.api.nvim_win_get_buf(winid)
     local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
