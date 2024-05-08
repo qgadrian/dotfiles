7a1e2fbda0bdb6c2624b66607e5bcd31b9f63886
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-05-08 20:23:03 +0200

    Add ai tools

---
+++ ai/install.sh
@@ -0,0 +1,16 @@
+read -p "Install ai LLM related tools (ollama)? " -n 1 -r; echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    brew install ollama
+
+    # ollama serve
+    # ollama run llama2
+    # ollama run llama2-uncensored
+fi
+
+# ai diffusion related tools (automatic1111)?
+#
+# git clone git@github.com:AUTOMATIC1111/stable-diffusion-webui.git
+# brew install cmake protobuf wget
+# asdf local python 3.10.9
+# pip3 install virtualenv
+# ./webui.sh
