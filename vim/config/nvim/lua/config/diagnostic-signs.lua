5b542bf3a9f06b944b43ea4b4788261b9d00bad8
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-06-21 15:12:56 +0200

    Update diagnostic signs

--- vim/config/nvim/lua/config/diagnostic-signs.lua
+++ vim/config/nvim/lua/config/diagnostic-signs.lua
@@ -7,5 +7,23 @@ local signs = {
 
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+-- exit with q on these buffers
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "checkhealth",
+    "fugitive*",
+    "git",
+    "help",
+    "lspinfo",
+    "netrw",
+    "notify",
+    "qf",
+    "query",
+  },
+  callback = function()
+    vim.keymap.set("n", "q", vim.cmd.close, { desc = "Close the current buffer", buffer = true })
+  end,
+})
