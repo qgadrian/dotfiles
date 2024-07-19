c7809dba6c30e710d01e8b1bcc3a7428396e41f9
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-07-19 08:20:26 +0200

    Update nvim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,26 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local vim = vim
+local opt = vim.opt
+local g = vim.g
+
+vim.g.markdown_fenced_languages = {
+  "html",
+  "javascript",
+  "typescript",
+  "css",
+  "scss",
+  "lua",
+  "vim",
+  "sh",
+}
+
+vim.diagnostic.config({
+  float = { border = "rounded" }, -- add border to diagnostic popups
+})
+
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.smoothscroll = true
+end
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,10 +1,34 @@
-vim.keymap.set("n", "-", ":Neotree toggle<CR>", { noremap = true })
+vim.keymap.set("n", "-", ":Neotree toggle reveal<CR>", { noremap = true })
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    open_files_do_not_replace_types = { "trouble", "qf" },
     window = {
       position = "current",
+    },
+    filesystem = {
+      hijack_netrw_behavior = "open_current",
+      use_libuv_file_watcher = true,
+      follow_current_file = {
+        enable = true,
+      },
+      filtered_items = {
+        hide_hidden = true,
+        hide_dotfiles = true,
+        never_show = {
+          ".DS_Store",
+          "thumbs.db",
+        },
+        always_show_by_pattern = {
+          ".env*",
+        },
+      },
+    },
+    buffers = {
+      follow_current_file = {
+        enable = true,
+      },
     },
   },
 }
