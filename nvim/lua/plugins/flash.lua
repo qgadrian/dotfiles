cc8fc26e6040502bae8dff9e08168b82ae494a90
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-08-28 10:21:46 +0200

    Better colors for flash.vim

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_hl(0, "FlashLabel", { foreground = "#000000", background = "#ed7e9f" })
+vim.api.nvim_set_hl(0, "FlashCurrent", { foreground = "#000000", background = "#7eed82" })
+
+return {}
