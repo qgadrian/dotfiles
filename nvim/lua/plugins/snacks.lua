510791fe348da1dae567f90b65a179f3cb1d0ab9
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-12-06 15:32:21 +1100

    Update nvim dashboard
    Don't like it too much, looks weird but I consider it WIP

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,18 +2,38 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
       preset = {
-        ---@param keys snacks.dashboard.Item[]
-        keys = function(keys)
-          -- add LazyExtra before Lazy
-          for k, key in ipairs(keys) do
-            if key.action == ":Lazy" then
-              -- key.key = "l" -- we don't have multiple panes, so `l` is free
-              table.insert(keys, k, { icon = " ", desc = "Terminal", action = ":terminal", key = "t" })
-              break
-            end
-          end
-        end,
+        header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "t", desc = "Terminal", action = ":terminal" },
+          -- {
+          --   icon = " ",
+          --   key = "c",
+          --   desc = "Config",
+          --   action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          -- },
+          -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          -- { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          -- { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
       },
     },
   },
