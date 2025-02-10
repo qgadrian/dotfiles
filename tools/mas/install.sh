9d836c0ad649821d683be4281f6cb93b09db4da5
Adrian Quintas <qgadrian@users.noreply.github.com>
2025-02-10 15:24:35 +0100

    Format script file

--- tools/mas/install.sh
+++ tools/mas/install.sh
@@ -1,5 +1,4 @@
-if ! command -v mas &> /dev/null
-then
+if ! command -v mas &>/dev/null; then
   echo "mas could not be found, installing"
   brew install mas
 fi
@@ -7,21 +6,22 @@ fi
 function search_and_install() {
   SELECTED_APP=$(mas search "$1" | peco | head -n 1 | awk '{print $1}')
 
-  mas install $SELECTED_APP
+  mas install "$SELECTED_APP"
 }
 
 echo "You will need to confirm which apps from the store you want to install"
 echo "Please sign in in the AppStore first"
 
-read -p "Did you sign in in the AppStore? " -n 1 -r; echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    search_and_install "raw right away"
-    search_and_install "quick view calendar"
-    search_and_install "Pixelmator Pro"
-    search_and_install "Photomator"
-    search_and_install "Slack for Desktop"
-    search_and_install "Infuse Video Player"
-    search_and_install "WhatsApp Desktop"
-    search_and_install "Telegram"
-    search_and_install "MetaImage Unlimited"
+read -p "Did you sign in in the AppStore? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  search_and_install "raw right away"
+  search_and_install "quick view calendar"
+  search_and_install "Pixelmator Pro"
+  search_and_install "Photomator"
+  search_and_install "Slack for Desktop"
+  search_and_install "Infuse Video Player"
+  search_and_install "WhatsApp Desktop"
+  search_and_install "Telegram"
+  search_and_install "MetaImage Unlimited"
 fi
