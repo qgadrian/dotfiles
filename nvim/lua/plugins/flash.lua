ff69639b0000ed9bae3eaa875e68cbd3f82b3326
Adrian Quintas <qgadrian@users.noreply.github.com>
2025-02-10 14:42:06 +0100

    Update flash config

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,4 +1,27 @@
 vim.api.nvim_set_hl(0, "FlashLabel", { foreground = "#000000", background = "#ed7e9f" })
 vim.api.nvim_set_hl(0, "FlashCurrent", { foreground = "#000000", background = "#7eed82" })
 
-return {}
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          jump_labels = false,
+          multi_line = true,
+          highlight = {
+            backdrop = true,
+            matches = true,
+            groups = {
+              match = "#82d7ee",
+              current = "#7eed82",
+              label = "#ed7e9f",
+            },
+          },
+        },
+      },
+    },
+  },
+}
